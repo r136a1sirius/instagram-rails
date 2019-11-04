03e62ebffaacfe806bfa549ef394893e982ff53a
r136a1sirius <warrior.0f.light.dev@gmail.com>
2019-11-04 21:31:06 +0300

    secondary commit

---
+++ config/initializers/carrierwave.rb
@@ -0,0 +1,13 @@
+CarrierWave.configure do |config|
+        config.fog_credentials = {
+          provider:              'AWS',                        # required
+          aws_access_key_id:     ENV['AWS_ACCESS_KEY_ID'],                        # required unless using use_iam_profile
+          aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],                        # required unless using use_iam_profile
+        }
+        config.storage = :fog
+        config.permissions = 0666
+        config.cache_dir = "#{Rails.root}/tmp/"
+        config.fog_directory  = ENV['FOG_DIRECTORY']                                      # required
+        config.fog_attributes = { 'cache_control' => "max-age=#{365.days.to_i}" } # optional, defaults to {}
+      end
+      
